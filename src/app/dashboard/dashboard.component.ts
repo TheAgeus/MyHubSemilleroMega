61b367d868e0e8f0ebea28e90e66d21736190a2e
Agustin <ageus94@gmail.com>
2024-07-11 22:14:12 -0600

    finish

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { MediaListComponent } from '../media-list/media-list.component';
 import { NavComponent } from '../nav/nav.component';
+import { RouterModule } from '@angular/router';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, MediaListComponent, NavComponent],
+  imports: [CommonModule, MediaListComponent, NavComponent, RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
